099c2746612b5b660be1fbdd3d5cd9ba6cde4d53
israelwme <israelwme@gmail.com>
2020-10-01 14:35:33 +0300

    homework

---
+++ src/Employee.java
@@ -0,0 +1,28 @@
+public class Employee {
+
+    private String fio;
+    private String position;
+    private String email;
+    private int phone;
+    private int salary;
+    private int age;
+
+    public Employee(String fio, String position, String email, int phone, int salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void Show(){
+        System.out.println("ФИО: " +fio + " Должность: " + position + " email: " + email + " Телефон: " + phone + " Зарплата: " + salary + " Возраст: " + age);
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+        // Конструктор класса должен заполнять эти поля при создании объекта;
+        // Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+        // Создать массив из 5 сотрудников
+        // С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+
+        Employee[] employeeArray = new Employee[5];
+        employeeArray[0] = new Employee("Ivanov Ivan", "Engineer  ", "teste@gmail.com", 892312312, 5000, 31);
+        employeeArray[1] = new Employee("Ivanov Ivan", "Doctor    ", "testd@gmail.com", 892312314, 3500, 42);
+        employeeArray[2] = new Employee("Ivanov Ivan", "Driver    ", "testr@gmail.com", 892312315, 4000, 55);
+        employeeArray[3] = new Employee("Ivanov Ivan", "Programmer", "testp@gmail.com", 892312316, 3000, 49);
+        employeeArray[4] = new Employee("Ivanov Ivan", "Manager   ", "testm@gmail.com", 892312317, 4500, 52);
+
+        for(Employee i: employeeArray){
+            if(i.getAge()>40) i.Show();
+        }
+    }
+}
